b4719bb25c2b0095aa21c5e872b02b54571de946
David Turner <csusbdt@gmail.com>
2017-06-15 11:21:15 -0700

    X

--- video-gen-3/gen_something.cpp
+++ video-gen-3/gen_something.cpp
@@ -25,11 +25,14 @@ unsigned char frame[H][W][3];
 
 void clear_frame() { memset(frame, 1, sizeof(frame)); }
 void draw_rect(int x, int y, int w, int h, byte r, byte g, byte b);
+void draw_ascii(char c, int x, int y, byte r, byte g, byte b);
 
 void draw_frame(double t) {
 	clear_frame();
+	draw_ascii('a', 200, 200, 0xFF, 0xFF, 0xFF);
 	const double pps = 50; // pixels per second
 	draw_rect(0 + t * pps, 0 + t * pps, 10, 10, 0x00, 0xff, 0x00);
+	draw_rect(0 + W - pps * t, 0 + H - pps * t, 10, 10, 0xff, 0x00, 0x00);
 }
 
 // Load the ascii bit-font image.
@@ -41,19 +44,26 @@ void load_ascii() {
 		"-y "
 		"-hide_banner "
 		"-i ascii-2000x1512.png "
-		"-s:v 1000x756 "
+		"-video_size 1000x756 "
 		"-pix_fmt rgb24 "
 		"-f rawvideo "
-		"-t 1 "
 		"- ";
 
 	FILE * ascii_pipe = popen(cmd, "r");
 
-	int count = fread(ascii, 3, 1000 * 756, ascii_pipe);
-	if (count != 3 * 1000 * 756) {
-		cout << "ERROR" << endl;
-		exit(1);
-	}
+	int i = 0;
+	while (true) {
+		int count = fread(ascii + i, 1, 1000 * 756 * 3 - i, ascii_pipe);
+cout << "******************* " << count << endl;
+		if (count == 0) {
+			if (i != 1000 * 756 * 3) {
+				cout << "ERROR.  Unexpected ascii file size of " << i << endl;
+				exit(1);
+			}
+			break;
+		}
+	} 
+
 	fflush(ascii_pipe);
 	pclose(ascii_pipe);
 }
@@ -77,6 +87,22 @@ void draw_rect(int x, int y, int w, int h, byte r, byte g, byte b) {
 			frame[y][x][0] = r;
 			frame[y][x][1] = g;
 			frame[y][x][2] = b;
+		}
+	}
+}
+
+void draw_ascii(char c, int x, int y, byte r, byte g, byte b) {
+	int x0 = x;
+	int x1 = x + 100;
+	int y0 = y;
+	int y1 = y + 100;
+	clamp(&x0, &y0);
+	clamp(&x1, &y1);
+	for (int y = y0; y < y1; ++y) {
+		for (int x = x0; x < x1; ++x) {
+			frame[y][x][0] = ascii[140 + y][90 + x][0];
+			frame[y][x][1] = ascii[140 + y][90 + x][1];
+			frame[y][x][2] = ascii[140 + y][90 + x][2];
 		}
 	}
 }
